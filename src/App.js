a1e087a73d5309414892b961de4ae5b0ba0cc009
Tamuz Paran <tamuzp@gmail.com>
2020-06-24 18:29:48 +0300

    commit test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tamuz Paran's website.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
